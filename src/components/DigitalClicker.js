70e9db342320912c1d08b8c9653a08eae83095ff
Andrzej Nicpoń <andrzejmd66@gmail.com>
2018-04-23 18:52:41 +0000

    automatically backed up by learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,14 @@
 // Code DigitalClicker Component Here
+import React from 'react'
+
+class DigitalClicker extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+    timesClicked = 0,
+    };
+  }
+
+
+  
+}
